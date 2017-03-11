e6ea71db2be59f7eecace2e61494c705592c4abb
minbo <hemin_it@163.com>
2017-03-12 00:22:20 +0800

    update

---
+++ decompress.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var zlib = require('zlib');
+
+fs.createReadStream('input.txt.gz').pipe(zlib.createGunzip()).pipe(fs.createWriteStream('input.ext'));
+
+console.log('the task is done');
